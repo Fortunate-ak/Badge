7e4cc628dfae1d7eefc10f2d1cec2d1a0ee032c0
TawanaState <tawmuks@gmail.com>
2025-10-24 12:55:27 +0200

    Started working on dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
-import { Route, Routes } from 'react-router'
+import { Route, Routes, useNavigate } from 'react-router'
 import AuthRoutes from './pages/auth'
 import React from 'react'
 import { GetCurrentUser } from './utils/auth'
+import type { User } from './types'
 
 function App() {
 
@@ -19,14 +20,28 @@ function App() {
 
 function Home() {
   const [currentUser, setCurrentUser] = React.useState<any>(null);
+  const navigate = useNavigate()
   React.useEffect(() => {
     GetCurrentUser().then(user => {
       setCurrentUser(user);
+      console.log(user)
     });
   }, []);
-  return <div>
-    {currentUser ? `Logged in as ${currentUser.email}` : "Not logged in"}
-  </div>
+  return <main className='w-full h-full flex flex-row justify-center items-center bg-secondary'>
+    <div className="p-6 bg-background rounded-md flex flex-col gap-0">
+      <span className="text-subtle-text text-xs">
+        {currentUser ? `Welcome` : "Not logged in"}
+      </span>
+      <h1 className='font-bold text-lg'>{(currentUser as User)?.first_name} {(currentUser as User)?.last_name}</h1>
+      <p className='text-sm text-subtle-text'>
+        {currentUser?.bio}
+      </p>
+      {
+        currentUser ? <button onClick={() => navigate("/auth/")} className="tw-button">Logout</button> : <button className="tw-button" onClick={() => navigate("/auth/")}>Login</button>
+      }
+    </div>
+
+  </main>
 }
 
 export default App
---
+++ frontend/src/ui/layouts/dashboard.tsx
@@ -0,0 +1,13 @@
+
+
+export default function DashboardLayout() {
+    return <main className="w-full h-full p-4 grid-cols-[1fr_5fr_5fr] grid-rows-[100%] max-h-screen overflow-y-scroll">
+        <Nav />
+    </main>
+}
+
+function Nav() {
+    return <nav className="">
+
+    </nav>
+}
