f22452a38747d057aef1f1624c06716267e0a74e
TawanaState <tawmuks@gmail.com>
2026-01-24 16:47:27 +0200

    feat: Enhance DocumentCard UI with improved styling and category display

--- frontend/src/ui/document-card.tsx
+++ frontend/src/ui/document-card.tsx
@@ -43,14 +43,20 @@ export default function DocumentCard({ value }: { value: Document }) {
     return <div className="flex flex-col p-4 border border-border rounded-md bg-secondary group transition-all cursor-pointer">
         <div className="flex flex-row justify-between items-center w-full">
             <span className="mso filled text-6xl font-bold text-muted">picture_as_pdf</span>
-            <button onClick={() => deleteFunc()} popoverTarget={"menu-items-" + value.id} className="mso text-xl place-self-start" style={{ 'anchorName': '--menu-anchor-' + value.id } as any}>delete</button>
+            <button onClick={() => deleteFunc()} popoverTarget={"menu-items-" + value.id} className="mso text-xl place-self-start cursor-pointer" style={{ 'anchorName': '--menu-anchor-' + value.id } as any}>delete</button>
             {/*<div popover="auto" id={"menu-items-"+id} className="fixed *:text-sm top-[anchor(bottom)] left-[anchor(left)] bg-secondary border border-border p-2 rounded-md shadow-lg" style={{'positionAnchor' : '--menu-anchor-'+id} as any}>
                 <button onClick={() => console.log("Cliked yey!")}>Delete Now</button>
                 <button></button>
             </div>*/}
         </div>
         <h2 className="font-semibold line-clamp-1">{value.title}</h2>
-        <span className="text-xs">{value.type}</span>
-        {value.updated_at && <span className="text-xs">Uploaded on {new Date(value.updated_at).toLocaleDateString()}</span>}
+        {value.updated_at && <span className="text-xs mb-2 text-muted">Uploaded on {new Date(value.updated_at).toLocaleDateString()}</span>}
+        <div className="flex flex-row gap-1 -mb-1">
+            {
+                (value.document_categories_details || []).map((v, k) => {
+                    return <span key={k+"-doc-category"} className="tw-tag text-xs first-letter:uppercase lowercase">{v.name}</span>
+                })
+            }
+        </div>
     </div>
 }
