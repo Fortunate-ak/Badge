73e3a96abc6b8631cf482d292ff1d4d4a65f426f
TawanaState <tawmuks@gmail.com>
2026-01-27 13:42:28 +0200

    fix: Update path for Applicants navigation in institution dashboard

--- frontend/src/ui/layouts/dashboard.tsx
+++ frontend/src/ui/layouts/dashboard.tsx
@@ -63,7 +63,7 @@ function Navigations() {
     // institution
     else if (user && user.is_institution_staff && location.pathname.includes("institution")) return <>
         <NavItem title="Home (Opportunities)" icon="home" path="/institution" />
-        <NavItem title="Applicants" icon="group" path='/institution/applicants' />
+        <NavItem title="Applicants" icon="group" path='/institution/staff' />
         <NavItem title="Consent" icon="order_approve" path='/institution/consent' />
         <NavItem title="Profile" icon="person" path={`/institution/${user?.institution_details?.[0]?.id || ""}`} />
     </>
